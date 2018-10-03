99d660f0c2732784ef40f24eeae030a711fc8091
Gilbert Torchon <gilbert@noukod.com>
2018-10-03 18:32:27 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,3 +17,25 @@ class Anagram
     end
   end
 end
+# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word=word
+  end
+
+  def match(arr)
+    arr.each do |word|
+      letters_of_anagram=@word.split("").sort
+      letters_of_word=word.split("").sort
+      if(letters_of_anagram!=letters_of_word)
+        next
+      else 
+        return [word.join("")]
+      end
+    end
+    return []
+  end
+end
