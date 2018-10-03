75f824c6566fa2f2fa8c57a60e699ec5e9c849c9
Gilbert Torchon <gilbert@noukod.com>
2018-10-03 18:17:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,10 +10,9 @@ class Anagram
   def match(arr)
     all_words = arr.split(" ")
     all_words.each do |word|
-      letters_of_anagram=@word.split("").sort 
+      letters_of_anagram=@word.split("").sort
       letters_of_word=word.split("").sort
       letters_of_anagram==letters_of_word
-        
     end
   end
 end
