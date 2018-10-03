9b3e9996507e5ab3eae2ae26787c242b6da9ec4c
Gilbert Torchon <gilbert@noukod.com>
2018-10-03 18:17:00 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,19 @@
 # Your code goes here!
 class Anagram
 
-  attr_accessor :name
+  attr_accessor :word
 
-  def initialize(name)
-    @name=name
+  def initialize(word)
+    @word=word
+  end
+
+  def match(arr)
+    all_words = arr.split(" ")
+    all_words.each do |word|
+      letters_of_anagram=@word.split("").sort 
+      letters_of_word=word.split("").sort
+      letters_of_anagram==letters_of_word
+        
+    end
   end
 end
